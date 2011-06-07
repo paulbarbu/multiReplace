f6048c29881b3d2c15dcb615f4ae78c3c7a7603c
PauLLiK <paullik@yahoo.com>
2011-06-07 21:58:31 +0300

    added support for -p -c and -l arguments

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,37 @@ using namespace std;
 int main(int argc, char *argv[]){
 
     int argNum = 1;
-    string path = "", lang = "", config = "";
+    string path = "", lang = "", config = ""; //TODO: set defaults
 
     while((argNum < argc) && (argv[argNum][0] == '-')){
         string argument = argv[argNum];
 
+        /**
+         * Path to subsitle(s)
+         */
         if(argument == "-p"){
             path = argv[++argNum];
+        }
+        /**
+         * Path to config file
+         */
+        else if(argument == "-c"){
+            config = argv[++argNum];
+        }
+        /**
+         * Language to look for in config
+         */
+        else if(argument == "-l"){
+            lang =  argv[++argNum];
         }
         else{
             printf("Unknown argument\n");
             return ERR_ARG;
         }
+        argNum++;
     }
 
-    printf("%s\n", path.c_str());
+    printf("%s\n%s\n%s\n", path.c_str(), lang.c_str(), config.c_str());
 
     return OK;
 }
