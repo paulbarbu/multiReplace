1e05047205afb16103d8c3351eedb632feafd1c5
PauLLiK <paullik@yahoo.com>
2011-07-29 22:08:56 +0300

    implemented getNameByUid, getUidByName, setName, setUid

--- src/user.py
+++ src/user.py
@@ -1,29 +1,76 @@
+import pwd
+from functions import *
+
 class User(object):
     '''User blueprint
+
+    The name is independent from the UID, the only condition is that _name must
+    be set when calling getUidByName() and _uid must be set when calling
+    getNameByUid(), else NameNotSet or UidNotSet will be raised
     '''
 
-    def __init__(self):
-        '''Gets the user under the program is run
-
+    def __init__(self, uid = None, name = None):
+        '''
         self._name, user's name, string
         self._uid, user's UID, int
-        self._groups, list of groups the user belongs to
         '''
-        pass
+        self._uid = uid
+        self._name = name
 
-    def getUID(self):
-        '''Get the UID
+    def setName(self, name):
+        '''Set's the user's name
+        '''
+        self._name = name
+
+    def setUid(self, uid):
+        '''Sets the user's UID
+        '''
+        self._uid = uid
+
+    def getUidByName(self):
+        '''Get the UID of the user
+
+        @throws NameNotSet if trying to use this method before the user's name
+        is set
+        @throws KeyError when the user is not found
 
         Populate _uid and return it
         '''
 
-    def getGroups(self):
-        '''Gets the list of groups the user belongs to
+        if self._name:
+            try:
+                self._uid = pwd.getpwnam(self._name)[2]
+            except KeyError as detail:
+                raise detail
 
-        This method populates _groups and returns that list
-        '''
-        pass
+            return self._uid
+        else:
+            raise NameNotSet()
 
-    def belongsTo(self, group):
-        '''Check whether the user belongs to a group
+    def getNameByUid(self):
+        '''Get the user's name by his UID
+
+        @throws UIDNotSet if trying to use this method before the UID is set
+        @throws KeyError when the UID is not found
+
+        Populate _name and return it
         '''
+        if self._uid:
+            try:
+                self._name = pwd.getpwuid(self._uid)[0]
+            except KeyError as detail:
+                    raise detail
+
+            return self._name
+        else:
+            raise UidNotSet()
+
+class NameNotSet(Exception):
+    '''Raised when the user's name is needed, but not set
+    '''
+    pass
+
+class UidNotSet(Exception):
+    '''Raised when the UID is needed, but not set
+    '''
+    pass
