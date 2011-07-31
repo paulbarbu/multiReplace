1c430f11b59d008dcaae5b69deea72fbac8b98e7
PauLLiK <paullik@yahoo.com>
2011-07-31 19:56:33 +0300

    added exception postponing

--- src/collection.py
+++ src/collection.py
@@ -13,34 +13,49 @@ class Collection(object):
         for obj in objects:
             self._items.append(obj)
 
-    def map(self, callback, *args, **kargs):
+    def map(self, callback, postponeException = False, *args, **kargs):
         '''Call every callback on the objects in the collection
 
         @throws EmptyCollectionError if this method is called on an empty
         collection
 
         @param callback a function object to be called on the objects
+        @param postponeException bool, if set to True exceptions will be raised
+        after the iteration, else the exceptions will be raised during the
+        iteration
         @param *args an argument list for passing to the callback function
         @param *kargs a keyed argument list to pass the the callback
 
         @return a list with the retvals values
 
         Usage:
-        col.map(Path.getPerm)          this will call Path.getPerm() on every
-        item in the collection col
+        col.map(Path.getPerm)
+            -> this will call Path.getPerm() on every item in the collection col
 
-        col2.map(Path.make, True))      this will call Path.make(True) on every
-        item in the collection col2
+        col2.map(Path.make, True))
+            -> this will call Path.make(True) on every item in the collection col2
+
+        col.map(path.Path.make, postponeException = True, overwrite = True))
+            -> this will call map(postponeException = True) and
+            Path.make(overwrite = True), so if you want to postpone exceptions
+            you'll have to use only keyword arguments
         '''
 
         result = []
+        exceptions = []
 
         if self._items:
             for obj in self._items:
                 try:
                     result.append(callback(obj, *args, **kargs))
                 except Exception as detail:
-                    raise detail
+                    if postponeException:
+                        exceptions.append(detail)
+                    else:
+                        raise detail
+
+            for e in exceptions:
+                raise e
 
             return result
         else:
